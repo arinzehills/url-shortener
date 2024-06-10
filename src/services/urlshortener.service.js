d26eee43e8f5b9bc864bba942dcf5bcef7c1fe95
arinzehills <arinzehill@gmail.com>
2024-06-10 18:51:49 +0100

    Adding Functinality to saved encoded URl to memory using map

--- src/services/urlshortener.service.js
+++ src/services/urlshortener.service.js
@@ -4,6 +4,7 @@ const urls = new Map();
 const encode = (longUrl) => {
   const id = generateId();
   console.log("ID Generated", id);
+  urls.set(id, { longUrl, visits: 0 });
   return `http://short.est/${id}`;
 };
 
