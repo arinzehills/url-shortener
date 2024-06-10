da2131d45ce6a8c07c787a5260b8e627dfe5d2d3
arinzehills <arinzehill@gmail.com>
2024-06-10 19:15:55 +0100

    Updated controller and servieces to decode the shortUrll and test passing

--- src/controllers/urlshortener.controller.js
+++ src/controllers/urlshortener.controller.js
@@ -6,13 +6,13 @@ const encode = (req, res) => {
     return res.status(400).json({ message: "Url is required" });
   }
   console.log(longUrl);
-  const shortUrl = urlService.encode(longUrl);
+  const shortUrl = urlService.encodeService(longUrl);
   console.log("Short Url Produced");
   res.json({ shortUrl });
 };
 const decode = (req, res) => {
   const { shortUrl } = req.body;
-  const longUrl = urlService.decode(shortUrl);
+  const longUrl = urlService.decodeService(shortUrl);
   res.json({ longUrl });
 };
 module.exports = { encode, decode };
--- src/services/urlshortener.service.js
+++ src/services/urlshortener.service.js
@@ -1,11 +1,19 @@
 const { generateId } = require("../utils/idGenerator");
 const urls = new Map();
 
-const encode = (longUrl) => {
+const encodeService = (longUrl) => {
   const id = generateId();
   console.log("ID Generated", id);
   urls.set(id, { longUrl, visits: 0 });
   return `http://short.est/${id}`;
 };
-
-module.exports = { encode };
+const decodeService = (shortUrl) => {
+  const id = shortUrl.split("/").pop();
+  const urlData = urls.get(id);
+  if (urlData) {
+    urlData.visits += 1;
+    return urlData.longUrl;
+  }
+  return null;
+};
+module.exports = { encodeService, decodeService };
