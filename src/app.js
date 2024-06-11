4bfe68bd78ecc863fd2a7812aa3aa09fa9f7d881
arinzehills <arinzehill@gmail.com>
2024-06-11 07:11:29 +0100

    Added Middleware error handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const urlRoutes = require("./routes/url.route");
+const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.json());
 app.use("/api", urlRoutes);
 
+app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port:: http://localhost:${PORT}/`);
